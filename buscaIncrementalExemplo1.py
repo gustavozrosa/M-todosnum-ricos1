ccb73ad9c06ef5d34d9ba2374e4b2258336f5df9
gustavozrosa <gustavozrosa1002@gmail.com>
2026-03-31 16:36:33 -0300

    primeiro commit

---
+++ buscaIncrementalExemplo1.py
@@ -0,0 +1,15 @@
+import numpy as np
+import math
+
+def f(x):
+    return np.sin(10*x) + np.cos(3*x)
+
+# Vetor
+n = 10
+x = np.linspace(3,6,n)
+y= f(x)
+
+print("y = ",y)
+
+"""#Busca Incremental
+for i in range()"""
